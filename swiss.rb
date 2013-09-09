97cdd871529b0538de74f1f0826d467b07233d2d
Nate Berkopec <nate.berkopec@gmail.com>
2013-09-09 11:04:25 -0400

    Basic DCI-compliant version w/o tiebreakers

--- swiss.rb
+++ swiss.rb
@@ -9,7 +9,7 @@ class Swiss
     self.current_round = 1
     puts "Number of players?"
     self.players = gets.chomp.to_i.times.map do |i|
-      puts "Name of player #{i}?"
+      puts "Name of player #{i + 1}?"
       name = gets.chomp
       name = nil if name.empty?
       Player.new(:name => name)
@@ -58,13 +58,8 @@ class Swiss
 
   def announce_winners
     puts_announcement "Final Scores:"
-    final_scores = players.values.map do |player|
-      match_score = player.match_points
-      game_score = player.game_points
-      [player[:name], (game_score + 0.1*match_score)]
-    end
-    final_scores.sort_by{ |i| i[1] }.reverse.each_with_index do |player, index|
-      puts "#{index + 1}. #{player[0]} :: (#{player[1]})"
+    players.sort_by{ |p| p.match_points }.reverse.each_with_index do |player, index|
+      puts "#{index + 1}. #{player.name} :: (#{player.match_points})"
     end
   end
 
@@ -92,66 +87,16 @@ class Swiss
   end
 
   def regular_round
-    carryover = nil
-    # pair players with similar scores
-    unassigned_players = players.map do |k,v|
-      [k, v[:game_points]]
-    end.sort_by{ |i| i[1] }.reverse
-
-    while unassigned_players.length > 1 do
-      if carryover
-        p = unassigned_players[1]
-        eligible_pool = unassigned_players.select do |eligible|
-          eligible[1] == p[1]
-        end
-
-        potential_pairing = nil
-
-        eligible_pool.each do |eligible|
-          potential_pairing = [carryover[0], eligible[0]]
-          unless pair_has_already_played?(potential_pairing)
-            current_pairs << potential_pairing
-            eligible_pool.delete(eligible)
-            unassigned_players.delete(eligible)
-            unassigned_players.delete(carryover)
-            carryover = nil
-            break
-          end
-        end
+    # pair players with the same match points against each other randomly
+    # DCI says you do not use tiebreakers when pairing
+    sorted_players = players.sort do |p1,p2|
+      if p1.match_points != p2.match_points
+        p1.match_points <=> p2.match_points
       else
-        p = unassigned_players.first
-        eligible_pool = unassigned_players.select do |eligible|
-          eligible[1] == p[1]
-        end
+        rand(100) <=> rand(100)
       end
-
-
-      while eligible_pool.length > 1
-        pairing = eligible_pool.sample(2)
-        numeric_pairing = pairing.map{ |t| t[0] }
-
-        # don't rematch
-        next if pair_has_already_played?(numeric_pairing)
-
-        current_pairs << numeric_pairing
-        pairing.each do |selected|
-          eligible_pool.delete(selected)
-          unassigned_players.delete(selected)
-        end
-      end
-
-
-      if eligible_pool.length == 1
-        carryover = eligible_pool.first
-      end
-
-      #puts "unassigned player: #{unassigned_players.length}"
     end
-
-    if unassigned_players.length == 1
-      current_pairs << [unassigned_players.first[0]]
-    end
-
+    self.current_pairs = sorted_players.shuffle.each_slice(2).to_a
     announce_pairings
     retrieve_scores
   end
@@ -169,15 +114,6 @@ class Swiss
     used_pairs = used_pairs.to_a + self.current_pairs.clone
     current_pairs = []
     announce_scores
-  end
-
-  def pair_has_already_played?(pairing)
-    used_pairs.each do |used_pair|
-      if used_pair.sort == pairing.sort
-        return true
-      end
-    end
-    return false
   end
 
 end
